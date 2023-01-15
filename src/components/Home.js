6af7810e5758974cc4fed6dcec19be60d5b38a06
nthokiesther09 <nthokiesther09@gmail.com>
2023-01-15 11:43:18 +0300

    completed assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+const colors = {color:'firebrick'}
 function Home() {
+ 
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+  <div id="home">
+    
+      <h1 style={colors}>{name} is a webdeveloper from {city}</h1>
+    
+  </div>
+  );
+
 }
 
 export default Home;
